15bb098e282be220c7a219e8328fd27cd888f54e
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-08-13 15:32:51 +0530

    Time: 1 ms (95.74%), Space: 47 MB (25.53%) - LeetHub

---
+++ 3683-earliest-time-to-finish-one-task/3683-earliest-time-to-finish-one-task.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int earliestTime(int[][] tasks)
+    {
+        int sum = tasks[0][0] + tasks[0][1];
+
+        for (int i = 1; i < tasks.length; i++)
+        {
+            int new_sum = tasks[i][0] + tasks[i][1];
+
+            if (new_sum < sum)
+            {
+                sum = new_sum;
+            }
+        }
+
+        return sum;
+    }
+}
