78fb611456aacf608572081d3b438d8cdbfdb52d
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-08-14 22:05:56 +0530

    Time: 6 ms (52.09%), Space: 55.7 MB (32.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,7 @@
-import java.util.HashMap;
-
 class Solution {
     public int majorityElement(int[] nums) {
-
+        Arrays.sort(nums);
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-
-            int count = map.getOrDefault(num, 0) + 1;
-            map.put(num, count);
-
-            if (count > n / 2) {
-                return num;
-            }
-        }
-
-        return -1;
+        return nums[n/2];
     }
 }
