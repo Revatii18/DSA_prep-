f9bd862c1d4f34e5047f78a71b1e2d8ccf1b9e46
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-08 12:40:14 +0530

    Time: 1 ms (99.97%), Space: 42.6 MB (45.41%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,36 @@
+class Solution 
+{
+    public int reverse( int x )  // 123 --> 321 
+    {
+       
+       int rev = 0 ;
+
+       while (x != 0)
+       {
+        int digit = x % 10 ;
+       x = x / 10 ;
+        
+       
+
+       if ( rev > Integer.MAX_VALUE / 10 || 
+             rev == Integer.MAX_VALUE/10 && digit > 7 || 
+              rev < Integer.MIN_VALUE / 10 ||
+               rev < Integer.MIN_VALUE/10 && digit < -8
+          )
+               { return 0 ;
+
+               }
+               else 
+               {
+                rev = rev * 10 + digit ;
+               }
+       }
+   return rev ;
+    }
+   
+}   
+
+
+
+
+    
