642822a497062cd417bbc3d186cdbf31ee9a8eac
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-08 11:31:07 +0530

    Time: 6 ms (21.66%), Space: 45.8 MB (86.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution
+ {
+    public boolean isPalindrome(int x)
+{
+    int orignal = x ;
+    int rev = 0 ;
+
+    if (x < 0)
+    {
+        return false ;
+    }
+    while (x != 0)
+    {
+        int digit = x % 10 ;
+        rev = rev * 10 + digit ;
+        x = x / 10 ;
+    }
+    if (orignal == rev)
+    {
+        return true ;
+    }
+
+    return false ;
+}
+ }   
