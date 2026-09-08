1a7b4c499fad92d5ced82b940255ef41c1786a48
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-08 22:35:33 +0530

    Time: 0 ms (100%), Space: 53.1 MB (21.81%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int partner = (mid % 2 == 0) ? mid + 1 : mid - 1;
+            if (nums[mid] == nums[partner]) {
+                left = Math.max(mid, partner) + 1;
+            } else {
+                right = Math.min(mid, partner);
+            }
+        }
+        return nums[left];
+    }
+}
