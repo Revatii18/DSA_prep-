54fc84997b38141003228e96e40927a6d05633bf
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-09 10:22:10 +0530

    Time: 1 ms (96.53%), Space: 47.7 MB (42.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,33 +1,52 @@
 class Solution {
-    public void setZeroes(int[][] matrix)
-    {
-         int m = matrix.length;
-        // Get number of columns
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
         int n = matrix[0].length;
 
-        // Create row marker array
-        boolean[] row = new boolean[m];
-        // Create column marker array
-        boolean[] col = new boolean[n];
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
 
-        // First pass: mark rows and columns that need to be zeroed
+        // Phase 0a: check if first row originally has a zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Phase 0b: check if first column originally has a zero
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // If element is zero, mark its row and column
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Phase 1: use first row/col as markers for the REST of the matrix
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+                    matrix[i][0] = 0; // mark row i
+                    matrix[0][j] = 0; // mark column j
                 }
             }
         }
 
-        // Second pass: set cells to zero based on markers
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // If the row or column is marked, set cell to zero
-                if (row[i] || col[j]) {
+        // Phase 2: zero out cells based on markers (skip row 0 / col 0 for now)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-    }}
+
+        // Phase 3: apply the saved first-row / first-col flags LAST
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) matrix[0][j] = 0;
+        }
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
+        }
+    }
+}
