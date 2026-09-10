79d7459d268f512ec3d1b1069dd3c9d807e4f29f
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-10 10:32:02 +0530

    Time: 2 ms (99.95%), Space: 52.6 MB (40.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,20 +4,64 @@ class Solution
     {
         List<Integer> list = new ArrayList<>();
 
-        Arrays.sort(nums);
+        int candidate1 = 0;
+        int candidate2 = 0;
 
-        int n = nums.length;
-        int window = n / 3;
+        int count1 = 0;
+        int count2 = 0;
 
-        for (int i = 0; i + window < n; i++)
+        // Find two possible candidates
+        for (int i = 0; i < nums.length; i++)
         {
-            if (nums[i] == nums[i + window])
+            if (nums[i] == candidate1)
             {
-                if (!list.contains(nums[i]))
-                {
-                    list.add(nums[i]);
-                }
+                count1++;
             }
+            else if (nums[i] == candidate2)
+            {
+                count2++;
+            }
+            else if (count1 == 0)
+            {
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if (count2 == 0)
+            {
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verify the candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums)
+        {
+            if (num == candidate1)
+            {
+                count1++;
+            }
+            else if (num == candidate2)
+            {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.length / 3)
+        {
+            list.add(candidate1);
+        }
+
+        if (count2 > nums.length / 3)
+        {
+            list.add(candidate2);
         }
 
         return list;
