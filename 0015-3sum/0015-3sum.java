c97896d833dbaf3a08a010a20fdb56c4f754e417
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-11 11:20:42 +0530

    Time: 26 ms (99.64%), Space: 59.2 MB (59.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -8,6 +8,7 @@ class Solution {
         int n = nums.length ;
         for (int i = 0 ; i < n-2 ; i++)
         {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i]== nums[i-1] )
             { continue ; }
 
