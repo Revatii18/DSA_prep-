248fe6bca7cb04f88b8eebc8edf2a675b7fa31ac
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-11 10:55:45 +0530

    Time: 27 ms (98.79%), Space: 59.1 MB (59.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,34 @@
-class Solution 
-{
-    public List<List<Integer>> threeSum(int[] nums) 
-    {
-Set<List<Integer>> ans = new HashSet<>();
+import java.util.*;
 
-        // First loop for first element
-        for (int i = 0; i < nums.length; i++) {
-            // Set to store elements seen in this iteration
-            Set<Integer> hashset = new HashSet<>();
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
 
-            // Second loop for second element
-            for (int j = i + 1; j < nums.length; j++) {
-                // Calculate third element needed
-                int third = -(nums[i] + nums[j]);
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate values for the fixed element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-                // If third already in set, we found a triplet
-                if (hashset.contains(third)) {
-                    List<Integer> temp = Arrays.asList(nums[i],nums[j], third);
-                    Collections.sort(temp);
-                    ans.add(temp);
+            // Small optimization: if smallest possible sum > 0, no more triplets possible
+            if (nums[i] > 0) break;
+
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
                 }
-
-                // Add current element to set
-                hashset.add(nums[j]);
             }
         }
-
-        // Convert set to list
-        return new ArrayList<>(ans);
-    }    
+        return result;
+    }
 }
