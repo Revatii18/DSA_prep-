12155e02b0880204f403a19c2d2048125d30ac25
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-11 11:19:38 +0530

    Time: 33 ms (70.87%), Space: 59.2 MB (45.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum(int[] nums) 
+    {
+        List < List <Integer >> result = new ArrayList <> () ;
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
+        int n = nums.length ;
+        for (int i = 0 ; i < n-2 ; i++)
+        {
+            if (i > 0 && nums[i]== nums[i-1] )
+            { continue ; }
 
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate values for the fixed element
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i +1 ;
+            int k = n -1 ;
 
-            // Small optimization: if smallest possible sum > 0, no more triplets possible
-            //if (nums[i] > 0) break;
-
-            int j = i + 1, k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
+            while (j < k)
+            {
+                int sum = nums[i] +nums[j]+nums[k];
+                if (sum < 0)
+                { j++ ;}
+                else if (sum > 0)
+                { k-- ;}
+                else 
+                {
+                    result.add(Arrays.asList(nums[i] , nums[j] , nums[k]));
+                    j++ ; k-- ;
                     while (j < k && nums[j] == nums[j - 1]) j++;
                     while (j < k && nums[k] == nums[k + 1]) k--;
                 }
             }
         }
-        return result;
+          return result ;
+
+
+        
     }
 }
