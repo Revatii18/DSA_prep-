ad333c4d7aa6e19565fb36b9590d862d36fa7701
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-14 22:11:54 +0530

    Time: 1 ms (98.67%), Space: 45.5 MB (16.28%) - LeetHub

--- 3452-sum-of-good-numbers/3452-sum-of-good-numbers.java
+++ 3452-sum-of-good-numbers/3452-sum-of-good-numbers.java
@@ -2,34 +2,34 @@ class Solution
 {
     public int sumOfGoodNumbers(int[] nums, int k) 
     {
-         int sum = 0 ;
+         int sum  = 0 ;
          int n = nums.length ;
          boolean isGoodSum ;
-      for ( int i = 0 ; i <= n-1 ; i ++ )
-   {
-    isGoodSum = true ; 
-     
-     if ( i - k >= 0 ) 
-     {
-        if (nums[i] <= nums[i-k]) 
-        {
-           isGoodSum = false;
-        }
-     }
-     if (i + k <= n -1  )
-      {
-         if (nums[i] <= nums[i+k]) 
+
+         for (int i = 0 ; i < n; i ++)
          {
-           isGoodSum = false;
-          }
+            isGoodSum = true ;
+            if ( (i - k) >= 0)
+            {
+                if (nums[i] <=  nums[i - k] )
+                {
+                    isGoodSum = false ;
+                }
+            }
+            if ( (i  + k) <=  n - 1)
+            {
+                if (nums[i] <=  nums[i + k] )
+                {
+                    isGoodSum = false ;
+                }
+            }
+            if(isGoodSum)
+            {
+                sum += nums[i];
+            }
+           
 
-     }
-  if (isGoodSum)
-{
-sum += nums[i];
-}}
-
-return sum ;
-
+         }
+ return sum ;
     }
 }
