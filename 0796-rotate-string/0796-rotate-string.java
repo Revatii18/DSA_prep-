cf57ccd1d8d87aee40c218e37b9e165d46165bb1
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-16 23:02:45 +0530

    Time: 1 ms (98.45%), Space: 43.5 MB (49.87%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,16 +2,13 @@ class Solution
 {
     public boolean rotateString(String s, String goal) 
     {
-         if(s.length() == goal.length() )
-     {
-         String ss = s + s ;
-           if ( (ss).contains(goal) )
-         {
-            return true ; 
-         }
-         else return false ;
+        if (s.length() != goal.length())
+        {
+            return false;
+        }
+
+        String ss = s + s;
+
+        return ss.contains(goal);
     }
-      else return false ;
-    }
-}
-    
+}  
