275a9ab0a37fd7ac732ecba9309d8699af03118c
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-16 09:45:19 +0530

    Time: 21 ms (28.27%), Space: 48.6 MB (5.74%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,48 @@
+class Solution
+{
+    public int minEatingSpeed(int[] piles, int h)
+    {
+        int maxPile = 0;
+
+        for (int bananas : piles)
+        {
+            maxPile = Math.max(maxPile, bananas);
+        }
+
+        int low = 1;
+        int high = maxPile;
+        int ans = maxPile;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            long totalH = calculateTotalHours(piles, mid);
+
+            if (totalH <= h)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public long calculateTotalHours(int[] piles, int speed)
+    {
+        long totalH = 0;
+
+        for (int bananas : piles)
+        {
+            totalH += (long) Math.ceil((double) bananas / speed);
+        }
+
+        return totalH;
+    }
+}
+    
