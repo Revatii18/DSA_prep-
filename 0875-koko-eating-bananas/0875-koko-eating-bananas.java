246637d633faab55c3732481fcc0b5c963255f0c
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-16 10:13:01 +0530

    Time: 10 ms (53.49%), Space: 47.8 MB (73.44%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,28 +1,33 @@
-class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
+import java.util.*;
+
+class Solution
+{
+    public int minEatingSpeed(int[] piles, int h)
+    {
         int left = 1;
         int right = Arrays.stream(piles).max().getAsInt();
-        
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (canEatAll(piles, mid, h)) {
-                right = mid;
-            } else {
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            long hours = 0;
+
+            for (int i = 0; i < piles.length; i++)
+            {
+                hours += (piles[i] + mid - 1) / mid;
+            }
+
+            if (hours <= h)
+            {
+                right = mid - 1;
+            }
+            else
+            {
                 left = mid + 1;
             }
         }
-        
+
         return left;
-    }
-    
-    private boolean canEatAll(int[] piles, int speed, int h) {
-        int time = 0;
-        for (int pile : piles) {
-            time += (pile - 1) / speed + 1; // calculate the time required to eat this pile
-            if (time > h) {
-                return false; // if the total time is greater than h, return false
-            }
-        }
-        return true; // if all piles can be eaten within h hours, return true
     }
 }
