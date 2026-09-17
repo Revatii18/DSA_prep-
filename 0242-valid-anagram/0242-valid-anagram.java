285011880b2a596593e7a0867f262a3e2afa7eb7
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-17 15:08:33 +0530

    Time: 1 ms (99.84%), Space: 44.6 MB (55.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
-        int[] count = new int[26];
+        int count[] = new int [26];
 
-for (char ch : s.toCharArray())
-{
-    count[ch - 'a']++;
-}
+        for( char ch : s.toCharArray())
+        {
+            count[ch - 'a']++ ;
+        }
+        for( char ch : t.toCharArray())
+        {
+            count[ch - 'a']-- ;
+        }
 
-for (char ch : t.toCharArray())
-{
-    count[ch - 'a']--;
-}
-
-for (int value : count)
-{
-    if (value != 0)
-    {
-        return false;
-    }
-}
-
-return true;
+        for(int values : count)
+        {
+            if( values  != 0)
+            {
+                return false ;
+            }
+            
+        }
+        return true ;
     }
 }
