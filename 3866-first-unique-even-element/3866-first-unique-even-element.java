223a9dc615fcc3d77c103e06531ed1d14c841f7c
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-23 23:24:27 +0530

    Time: 3 ms (31.39%), Space: 46.1 MB (72.88%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public int firstUniqueEven(int[] nums) 
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (int i : nums)
+        {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        // Find first even number appearing once
+        for (int i : nums)
+        {
+            if (i % 2 == 0 && map.get(i) == 1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
