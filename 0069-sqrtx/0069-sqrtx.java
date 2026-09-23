3804f78a05e87b008239b2930de4c39ed872a943
Revati Sambhaji Tapkir <revatitapkir8@gmail.com>
2026-09-23 12:36:05 +0530

    Time: 82 ms (5.05%), Space: 42.7 MB (23.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,11 +2,6 @@ class Solution
 {
     public int mySqrt(int x) 
     {
-        if (x == 0 || x == 1)
-        {
-            return x;
-        }
-
         int low = 1;
         int high = x;
         int ans = 0;
@@ -16,11 +11,7 @@ class Solution
             int mid = low + (high - low) / 2;
             long square = (long) mid * mid;
 
-            if (square == x)
-            {
-                return mid;
-            }
-            else if (square < x)
+            if (square <= x)
             {
                 ans = mid;
                 low = mid + 1;
